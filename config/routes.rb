55a89d3fdac55dae5ccde38a56c00739a3235c32
Betsy Zhang <160428307+eastbetsy@users.noreply.github.com>
2025-04-29 20:26:39 +0000

    Add the chat action to app/controllers/pages_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'dashboard', to: 'pages#dashboard'
   get 'chat', to: 'pages#chat'
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+
 end
