58bc59abdd1d5008db754a8b3e2f96710ea6b649
Betsy Zhang <160428307+eastbetsy@users.noreply.github.com>
2025-04-29 20:38:19 +0000

    define remaining routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'chat', to: 'pages#chat'
   get 'dashboard', to: 'pages#dashboard'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'register', to: 'pages#signup'
+  get 'resources', to: 'pages#resources'
 end
