d617060792672dc826b09d0bfdcad96e2dd6f49f
Betsy Zhang <160428307+eastbetsy@users.noreply.github.com>
2025-05-08 19:44:19 +0000

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # Pages routes
   root 'pages#home'
   get 'chat', to: 'pages#chat'
   get 'dashboard', to: 'pages#dashboard'
@@ -6,10 +7,9 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   get 'signup', to: 'pages#signup'
   get 'resources', to: 'pages#resources'
-end
 
-Rails.application.routes.draw do
-  root 'plaid#index'
+  # Plaid routes
+  get '/', to: 'plaid#index', as: :plaid_root
   post 'plaid/create_link_token', to: 'plaid#create_link_token'
   post 'plaid/exchange_public_token', to: 'plaid#exchange_public_token'
   get 'plaid/accounts', to: 'plaid#accounts'
